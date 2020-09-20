66b698e1e3ba5c25151b67ce64e22a675770fa77
unknown <jgeiser@uncc.edu>
2020-09-20 12:01:33 -0400

    console logged search input

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,6 +3,22 @@ var tableData = data;
 
 // Select the button and form
 var button = d3.select("#filter-btn");
-var form = d3.select("#form")
+var form = d3.select("#form");
 
-// read filter
+// Event handles
+button.on("click", runQuery);
+form.on("submit", runQuery);
+
+// Event handle function
+function runQuery() {
+    // Prevent page refresh on search
+    d3.event.preventDefault();
+
+    // read date filter
+    var dateElement = d3.select("#datetime");
+    console.log(dateElement);
+
+    var date = dateElement.property("value");
+    console.log(date);
+}
+
